d8b88c00428e3a7c3bd8b80bc06038febc7ce362
Yazhou Fu <fuyazhou80@hotmail.com>
2022-11-09 14:08:32 +0800

    多ite整合

--- casual_inference_miaosuan.py
+++ casual_inference_miaosuan.py
@@ -9,6 +9,9 @@ from catboost import CatBoostRegressor
 from sklearn import ensemble
 from sklearn.preprocessing import RobustScaler, StandardScaler
 from xgboost.sklearn import XGBClassifier, XGBRegressor
+from causalml.metrics.visualize import plot_lift, plot_qini, plot_gain, plot_tmlegain, auuc_score, qini_score, \
+    get_tmlegain
+from causalml.propensity import ElasticNetPropensityModel
 import pickle
 import argparse
 import logging
@@ -97,29 +100,29 @@ def inference(train, treatment_columns_category_dict, xgb, feature_columns,
         train_temp = train.copy(deep=True)
         if col in treatment_columns_common:
             pd_category = pd.DataFrame()
-            print("{} is category 变量，在common中 ".format(col))
+            logging.info("{} is category 变量，在common中 ".format(col))
             treat_enum = treatment_columns_category_dict[col]
-            print(treat_enum)
+            logging.info(treat_enum)
             for enum_value in treat_enum.keys():
-                print(enum_value)
+                logging.info(enum_value)
                 train_temp[col] = enum_value
                 pd_category[enum_value] = xgb.predict(
                     train_temp[feature_columns])
             d = dict(pd_category.mean())
             d = sorted(d.items(), key=lambda x: x[1], reverse=True)[0][0]
             treatment_cate_common[col] = treat_enum[d]
-            print("\n\n")
+            logging.info("\n\n")
         else:
             pd_category = pd.DataFrame()
-            print("{} is category 变量，不在common中 ".format(col))
+            logging.info("{} is category 变量，不在common中 ".format(col))
             treat_enum = treatment_columns_category_dict[col]
-            print(treat_enum)
+            logging.info(treat_enum)
             for enum_value in treat_enum.keys():
-                print(enum_value)
+                logging.info(enum_value)
                 train_temp[col] = enum_value
                 pd_category[enum_value] = xgb.predict(
                     train_temp[feature_columns])
-            print("\n\n")
+            logging.info("\n\n")
             cate_max = pd_category.max(axis=1)
             cate_idxmax = pd_category.idxmax(axis=1)
             pd_category[col] = cate_max
@@ -145,14 +148,25 @@ def inference(train, treatment_columns_category_dict, xgb, feature_columns,
             user_res[value["user_id"]] = temp
         else:
             user_res[value["user_id"]] = max_idx
-    #     print(max_idx)
+    #     logging.info(max_idx)
     result["user_treatment"] = user_res
 
     # 保存文件
     with open("causal_inference.pkl", "wb") as tf:
         pickle.dump(result, tf)
 
-    return result
+    return result, outcome_pd
+
+
+def get_auuc(pred_df, y_col, treatment_col):
+    auuc_df = pd.DataFrame(auuc_score(pred_df, outcome_col=y_col,
+                                      treatment_col=treatment_col, normalize=True)).reset_index()
+    auuc_df.columns = ['Learner', 'auuc']
+    auuc_df['Lift'] = (auuc_df['auuc'] /
+                       auuc_df[auuc_df.Learner == 'Random'].auuc.values) - 1
+    auuc_df = auuc_df.sort_values('auuc', ascending=False)
+    logging.info(auuc_df)
+    return auuc_df
 
 
 # mock data
@@ -212,15 +226,15 @@ if __name__ == "__main__":
         treatment_columns_category_dict, inference_data, xgb = data_process(train_data, inference_data, feature_columns,
                                                                             treatment_columns_category,
                                                                             outcome_column)
-        result = inference(inference_data, treatment_columns_category_dict, xgb, feature_columns,
-                           treatment_columns_category, treatment_columns_common, treatment_change_value,
-                           treatment_columns_continuous,
-                           userid_column)
+        result, outcome_dp = inference(inference_data, treatment_columns_category_dict, xgb, feature_columns,
+                                       treatment_columns_category, treatment_columns_common, treatment_change_value,
+                                       treatment_columns_continuous,
+                                       userid_column)
         logging.info("\n\n**********")
         logging.info("final result file is : causal_inference.pkl")
 
     except:
-        logging.warning('\n\n****something wrong****')
+        logging.warning("\n\n****something wrong****")
 
 ## example
 #  python casual_inference_miaosuan.py --feature_columns "['V_0', 'V_1', 'V_2', 'V_3', 'V_4', 'V_5', 'V_6', 'V_7', 'V_8', 'V_9','V_10', 'treatment']" --treatment_columns_category "['V_10', 'treatment']" --treatment_columns_continuous "{'V_6': [None, 10, 1],'V_7': [0.3, None, 0]}" --treatment_columns_common "['V_6','treatment']" --outcome_column "['outcome']" --userid_column "['user_id']"
